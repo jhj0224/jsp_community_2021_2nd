4bd8b614aeafc8ffb09d63a2c8d5a352a1310b05
jhj0224 <jhj8664@gmail.com>
2021-07-27 00:07:42 +0900

    메일 발송 테스트

--- src/main/java/com/jhs/exam/exam2/http/controller/UsrHomeController.java
+++ src/main/java/com/jhs/exam/exam2/http/controller/UsrHomeController.java
@@ -1,6 +1,7 @@
 package com.jhs.exam.exam2.http.controller;
 
 import com.jhs.exam.exam2.http.Rq;
+import com.jhs.exam.exam2.util.Ut;
 
 public class UsrHomeController extends Controller {
 	@Override
@@ -8,6 +9,9 @@ public class UsrHomeController extends Controller {
 		switch (rq.getActionMethodName()) {
 		case "main":
 			actionShowMain(rq);
+			break;
+		case "doSendMail":
+			actionDoSendMail(rq);
 			break;
 		default:
 			rq.println("존재하지 않는 페이지 입니다.");
@@ -18,4 +22,9 @@ public class UsrHomeController extends Controller {
 	private void actionShowMain(Rq rq) {
 		rq.jsp("usr/home/main");
 	}
+	
+	private void actionDoSendMail(Rq rq) {
+		Ut.sendMail("jhj8664@gmail.com", "gduelaaasdcnfrlv", "no-reply@hyungjoon.site", "준이의 코딩천국 알림", "jhj8664@gmail.com", "제목ㅋㄷ", "내용ㅋㄷ");
+	}
+	
 }
