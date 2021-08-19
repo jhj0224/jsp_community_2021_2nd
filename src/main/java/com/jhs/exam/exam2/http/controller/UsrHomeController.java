66cfa1573d681c45144933fe3f030a55edf6f880
jhj0224 <jhj8664@gmail.com>
2021-08-19 09:31:40 +0900

    UsrHomeController 주석 달기

--- src/main/java/com/jhs/exam/exam2/http/controller/UsrHomeController.java
+++ src/main/java/com/jhs/exam/exam2/http/controller/UsrHomeController.java
@@ -11,6 +11,7 @@ public class UsrHomeController extends Controller {
 		
 	}
 	
+	// ActionMethodName이 아래 case와 일치하면 해당 함수로 이동
 	@Override
 	public void performAction(Rq rq) {
 		switch (rq.getActionMethodName()) {
@@ -26,10 +27,12 @@ public class UsrHomeController extends Controller {
 		}
 	}
 
+	// main 페이지 보여주는 함수
 	private void actionShowMain(Rq rq) {
 		rq.jsp("usr/home/main");
 	}
 	
+	// Gmail 외부에서 불러서 이메일 보내는 함수
 	private void actionDoSendMail(Rq rq) {
 		App app = Container.app;
 		Ut.sendMail(app.getSmtpGmailId(), app.getSmtpGmailPw(), "no-reply@hyungjoon.site", "준이의 코딩천국 알림", "jhj8664@gmail.com", "제목ㅋㄷ", "내용ㅋㄷ");
