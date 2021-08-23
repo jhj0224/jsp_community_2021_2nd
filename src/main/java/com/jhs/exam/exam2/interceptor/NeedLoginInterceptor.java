b0ad2c93bfbe9c31b00d3a99ab858cdbf570caf0
jhj0224 <jhj8664@gmail.com>
2021-08-23 10:35:26 +0900

    interceptor 주석 달기

--- src/main/java/com/jhs/exam/exam2/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/jhs/exam/exam2/interceptor/NeedLoginInterceptor.java
@@ -8,12 +8,14 @@ public class NeedLoginInterceptor extends Interceptor {
 		
 	}	
 	
+	// ControllerTypeName 값이 "usr"가 아닐경우 true를 리턴
 	@Override
 	public boolean runBeforeAction(Rq rq) {
 		if ( rq.getControllerTypeName().equals("usr") == false ) {
 			return true;
 		}
 		
+		// getActionPath 값이 해당 case값일 경우 true를 리턴
 		switch (rq.getActionPath()) {
 		case "/usr/article/list":
 		case "/usr/article/detail":
@@ -31,6 +33,7 @@ public class NeedLoginInterceptor extends Interceptor {
 			return true;
 		}
 		
+		// 로그인 요구 메세지 출력후 로그인 페이지 이동 + 로그인페이지로 이동전 페이지 정보를 담아 이동하는 메서드
 		if ( rq.isNotLogined() ) {
 			rq.replace("로그인 후 이용해주세요.", "../member/login?afterLoginUri=" + rq.getEncodedAfterLoginUri());
 			
